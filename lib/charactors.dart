ce46c2eecb684cc10e1992a022ca65661a6420c7
yk <yek.soltani66@gmail.com>
2024-08-31 11:36:38 +0330

    complete

--- lib/charactors.dart
+++ lib/charactors.dart
@@ -6,10 +6,15 @@ import 'package:get/get.dart';
 import 'package:o3d/o3d.dart';
 import 'package:three_dimension_model/list_of_chars.dart';
 
-class Caharactors extends StatelessWidget {
-  Caharactors({super.key});
+class Chars extends StatefulWidget {
+  const Chars({super.key});
+
+  @override
+  State<Chars> createState() => Caharactors();
+}
+
+class Caharactors extends State<Chars> {
   O3DController o3 = O3DController();
-  var autoPlay = false.obs;
 
   @override
   Widget build(BuildContext context) {
@@ -22,74 +27,61 @@ class Caharactors extends StatelessWidget {
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: [Color(0xff5c6163), Color(0xff3d4244), Color(0xff383c3f), Color(0xff353a3c), Color(0xff2f3437), Color(0xff2a2f31)],
+            colors: [Color(0xfff0abbf),Color(0xffbc68a2), Color(0xff351c63), Color(0xff374cbc), Color(0xff3babe4), Color(0xff213d7f), ],
           ),
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    autoPlay.value = true;
-                    
-                  },
-                  child: Text('autoplay')),
-              GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                shrinkWrap: true,
-                physics: AlwaysScrollableScrollPhysics(),
-                scrollDirection: Axis.vertical,
-                itemCount: transformers.length,
-                itemBuilder: (context, index) {
-                  print(autoPlay.value);
-                  return Obx(() => Container(
-                        height: 300,
-                        width: 200,
-                        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                        // padding: const EdgeInsets.all(5.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xff33383b),
-                        ),
-                        child: Stack(
-                          children: [
-                            Positioned.fill(
-                              child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(30),
-                                  child: O3D.asset(
-                                    src: 'assets/three_d_models/transformers${transformers[index]['name'].toString()}.glb',
-                                    controller: o3,
-                                    autoPlay: autoPlay.value,
-                                    cameraOrbit: CameraOrbit(10, 90, 50),
-                                  )),
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              right: 0,
-                              left: 0,
-                              child: BlurryContainer(
-                                child: Center(
-                                  child: Text(
-                                    transformers[index]['name'].toString(),
-                                    style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                                blur: 36,
-                                height: 50,
-                                elevation: 6,
-                                // padding: const EdgeInsets.all(32),
-                                color: Colors.grey.withOpacity(0.15),
-                                padding: const EdgeInsets.all(8),
-                                borderRadius: const BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ));
-                },
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          shrinkWrap: true,
+          physics: AlwaysScrollableScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          itemCount: transformers.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 300,
+              width: 200,
+              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              // padding: const EdgeInsets.all(5.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                // color: Color(0xff6f315b),
               ),
-            ],
-          ),
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: O3D.asset(
+                          src: 'assets/three_d_models/transformers/${transformers[index]['name'].toString()}.glb',
+                          controller: o3,
+                          autoPlay: true,
+                          cameraOrbit: CameraOrbit(10, 90, 50),
+                        )),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    child: BlurryContainer(
+                      child: Center(
+                        child: Text(
+                          transformers[index]['name'].toString(),
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      blur: 36,
+                      height: 50,
+                      elevation: 6,
+                      // padding: const EdgeInsets.all(32),
+                      color: Colors.grey.withOpacity(0.15),
+                      padding: const EdgeInsets.all(8),
+                      borderRadius: const BorderRadius.only(bottomRight: Radius.circular(10), bottomLeft: Radius.circular(10)),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      home: Caharactors(),
+      home: Chars(),
     );
   }
 }
